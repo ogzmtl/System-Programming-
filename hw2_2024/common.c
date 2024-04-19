c324be10f453a5e47afefb4ae7ba0428ea11bcfe
oguz <oguzmutlu81@gmail.com>
2024-04-19 04:44:12 +0300

    All operations done, investigation: malloc issue

--- hw2_2024/common.c
+++ hw2_2024/common.c
@@ -11,10 +11,8 @@
 char* convertIntegerToString(const int* arr, const char delim, const int arr_size){
 
     int total_len=0; 
-// printf("HIIII\n");
     for(int i = 0; i < arr_size; i++){
         
-        //snprintf yazilacak toplam lengthi ifade etmektedir +1 eklenmesinin sebebi de delimeter icindir
         int num_bytes_written = snprintf(NULL, 0,"%d",arr[i]);
         total_len += num_bytes_written+1;
     }
@@ -23,12 +21,10 @@ char* convertIntegerToString(const int* arr, const char delim, const int arr_siz
 
     char* str = (char*)calloc(total_len,sizeof(char));
     int point = 0;
-    // printf("successfully malloc operation done 26\n");
 
 
     for(int i = 0; i < arr_size; i++){
         
-        //snprintf yazilacak toplam lengthi ifade etmektedir +1 eklenmesinin sebebi de delimeter icindir
         int num_bytes_written = snprintf(str+point,total_len-point,"%d",arr[i]);
         if(num_bytes_written < 0 || num_bytes_written > total_len - point)
         {
@@ -51,8 +47,6 @@ void convertStringArrayToInteger(char** str, int count, int*arr){
     for(int i = 0; i < count; i++){
         int val = convertSingleStringToInteger(str[i]);
         if(val < 0){
-            // free(str);
-            // free(arr);
             return;
             // exit(EXIT_FAILURE);
         }
@@ -104,7 +98,6 @@ int splitStringIntoArray_I(const char* str, const char delim, char** splitted, i
         {
             temp_element[inside_counter] = '\0';
             splitted[i] = (char*)calloc(inside_counter, sizeof(char));
-            // printf("successfuly done malloc 106\n");
             strcpy(splitted[i], temp_element);
             i++;
             memset(temp_element, 0, sizeof(temp_element));
@@ -134,12 +127,9 @@ char* splitStringIntoArray_Custom(const char* str, char** splitted){
         if(*tmp == ',' || *tmp == '!')
         {
             temp_element[inside_counter++] = '\0';
-            // printf("tempo :%s, %d\n", temp_element, inside_counter);
             if(*tmp == '!'){
                 
                 // compared = (char*)malloc(sizeof(char) * inside_counter);
-                // printf("successfuly done malloc 140\n");
-                // printf("temp element: %s\n", temp_element);
                 // strcpy(compared, temp_element);
                 for(int j = 0; j < inside_counter-1; j++){
                     compared[j] = temp_element[j];
@@ -148,13 +138,11 @@ char* splitStringIntoArray_Custom(const char* str, char** splitted){
             }
             else{
                 splitted[i] = (char*)calloc(inside_counter, sizeof(char));
-                // printf("successfuly done malloc 146\n");
                 // strcpy(splitted[i], temp_element);
                 for(int j = 0; j < inside_counter-1; j++){
                     splitted[i][j] = temp_element[j];
                 }
                 splitted[i][inside_counter-1] = '\0';
-                // printf("tempo element: %s\n", temp_element);
                 i++;
             }
             // temp_element[inside_counter] = '\0';
@@ -169,7 +157,7 @@ char* splitStringIntoArray_Custom(const char* str, char** splitted){
         }
         tmp++;
     }
-    printf("convertedArray x : %s \n", compared);
+    // printf("convertedArray x : %s \n", compared);
 
     return compared;
 
@@ -195,7 +183,6 @@ char* readOneByOne(const char* firstChildFifo){
     int numBytesRead = 0;
     int childBufferCounter = 0;
     char* buffer = (char*)calloc(32, sizeof(char));
-    // printf("successfuly done malloc 188\n");
 
     // char buffer[256];
     memset(buffer, 0, 6);
@@ -249,7 +236,6 @@ char* readOneByOne_Custom(const char* firstChildFifo, const char delim1, const c
     int numBytesRead = 0;
     int childBufferCounter = 0;
     char* buffer = (char*)calloc(12,sizeof(char));
-    // printf("successfuly done malloc 188\n");
     int delim_flag1 = 0;
     int delim_flag2 = 0;
 
@@ -262,7 +248,6 @@ char* readOneByOne_Custom(const char* firstChildFifo, const char delim1, const c
         free(buffer);
         exit(EXIT_FAILURE);
     }
-    // printf("open is not failed\n");
     for(;;)
     {
         char c; 
@@ -272,9 +257,7 @@ char* readOneByOne_Custom(const char* firstChildFifo, const char delim1, const c
         memset(log, 0, sizeof(log));
         fflush(stdout);
         numBytesRead = read(fd, &c, sizeof(c));
-        // printf("readde gelmedim %d-%d\n", c, numBytesRead);
         if(numBytesRead == 0){
-            // printf("readde gelmsedim %d %d\n", delim_flag1, delim_flag2);
             if(delim_flag1 && delim_flag2){
                 if(childBufferCounter+1 % 12 == 0){
                     int newSize = childBufferCounter + 12;
@@ -313,12 +296,7 @@ char* readOneByOne_Custom(const char* firstChildFifo, const char delim1, const c
                 buffer[childBufferCounter++] = c;
             }
             
-
         }
-        printf("Buffer LEn %d : \n", strlen(buffer));
-        // printf("flag 1 : %d\n", delim_flag1);
-        // printf("flag 2 : %d \n", delim_flag2);
-        // printf("X\n");
     }
     
     if(close(fd) == -1){
@@ -326,9 +304,7 @@ char* readOneByOne_Custom(const char* firstChildFifo, const char delim1, const c
         free(buffer);
         return NULL;
     }
-
-    // printf("AAAAAACCCXXCXXX\n");
-    write(STDOUT_FILENO, buffer, strlen(buffer));
+    // write(STDOUT_FILENO, buffer, strlen(buffer));
     return buffer;
 }
 
@@ -336,7 +312,6 @@ int check_command(const char** splitted, const char* command, int count){
 
     int flag=0;
     for(int i = 0; i < count; i++){
-        // printf("compare %s: %d\n", splitted[i], strlen(splitted[i]));
         if(strncmp(splitted[i], command, strlen(command)) == 0)
         {
             flag = 1;
--- hw2_2024/hw2.c
+++ hw2_2024/hw2.c
@@ -25,8 +25,6 @@ int addValuesInInteger(int* convertedArray, int size){
 
 void intoStringWithDelim(int sum, char* buffer, const char delim){
     int num_written  = sprintf(buffer, "%d", sum);
-    // printf("WRITTEN SIZE : %d\n", num_written);
-    int len = 0;
     buffer[num_written] = delim;
     buffer[num_written + 1] = '\0';
 }
@@ -40,7 +38,6 @@ long int multiplyValuesInInteger(long int* convertedArray, int size){
 }
 int multiplyOperation(long int* convertedArray, int count){
 
-
     long int result = multiplyValuesInInteger(convertedArray, count);
     return result;
 
@@ -53,9 +50,8 @@ int writeToFirstFifo(const char* str, int fd){
     int numBytesWritten = write(fd, str, strlen(str)+1);
     if(numBytesWritten == -1 )
     {
-        perror("write failed");
+        perror("write failed\n");
         return -1;
-        //fail oldugunda error code ver
     }
     return 1;
 }
@@ -68,29 +64,25 @@ int parseSecondfifo(const char* buffer, const char delim){
     int flag = 0;
     char* convertedArrayStr = NULL;
     int count = countHowManyElementsWillExtract(buffer, delim);
-    printf("Count of elements: %d\n", count);
+    // printf("Count of elements: %d\n", count);
     splitted = (char**)calloc(count, sizeof(char*));
     if (splitted == NULL) {
         printf("Memory allocation failed!\n");
         free(convertedArrayStr);
         return -1;
     }
-    printf("\n\nADKSADKLASDLK successfuly done malloc 157hw2\n");
 
     convertedArrayStr = splitStringIntoArray_Custom(buffer, splitted);
-    // convertedArray = (int*)calloc(count, sizeof(int));
-    // printf("successfuly done malloc 161hw2\n");
+
     for(int i = 0; i < count-1; i++){
         if(strncmp(splitted[i], "multiply", 8) == 0){
             flag = 1;
         }
-        // printf("ss necwcc: %d\n",strlen( splitted[i]));
         convertedArray[i] = convertSingleStringToInteger(splitted[i]);
     }
     if(strncmp(splitted[count-1], "multiply", 8) == 0){
         flag = 1;
     }
-    // printf("HIIII0 %d\n", flag);
     if(flag == 0){
         free(convertedArrayStr);
         // free(convertedArray);
@@ -100,17 +92,9 @@ int parseSecondfifo(const char* buffer, const char delim){
         free(splitted);
         return -1;
     }
-    // printf("HIIII2\n");
-    // for(int i = 0; i < count-1; i++){
 
-    //     printf("sas necwcc: %d\n",convertedArray[i]);
-    // }
-    // printf("HIIII3\n");
     long int res =  multiplyValuesInInteger(convertedArray, count-1);
-    // printf("HIIII4\n");
-    // for(int i = 0; i < count; i++){
-        printf("convertedArray : %s \n", convertedArrayStr);
-    // }
+
     int addRes = strtol(convertedArrayStr, &endptr, 10);
     // printf("HIIII5\n");
     printf("Multiplication result: %ld\n", res);
@@ -132,17 +116,18 @@ int parseFifo(const char* buffer, const char delim){
     int* convertedArray;
     int count = countHowManyElementsWillExtract(buffer, delim);
     splitted = (char**)calloc(count, sizeof(char*));
-    // printf("XXXXXX-------count: %d\n", count);
-    // printf("successfuly done malloc 212\n");
+    if(splitted == NULL){
+        return -1;
+    }
+
     convertedArray = (int*)calloc(count,sizeof(int));
+    if(convertedArray == NULL){
+        return -1;
+    }
     // printf("successfuly done malloc 215\n");
 
     splitStringIntoArray_I(buffer, delim, splitted, count);
     convertStringArrayToInteger(splitted, count, convertedArray);
-    // for(int i = 0; i < count; i++){
-
-    //     printf("hg int : %d\n", convertedArray[i]);
-    // }
 
     int sum = addValuesInInteger(convertedArray, count);
     
@@ -160,8 +145,15 @@ static void sigchldHandler(int sign){
     int childpid;
 
     savedErrno = errno;
+
+    sigset_t mask, prevMask;
+    sigemptyset(&mask);
+    sigaddset(&mask, SIGCHLD);
+    sigprocmask(SIG_BLOCK, &mask, &prevMask);
+    savedErrno = errno;
     while((childpid = waitpid(-1, &status, WNOHANG)) > 0){
         printf("handler: Reaped child %ld\n", (long) childpid);
+        signalCnt++;
         spawnedProcess--;
     }
     if (childpid == -1 && errno != ECHILD)
@@ -171,7 +163,7 @@ static void sigchldHandler(int sign){
         unlink(FIRST_CHILD_FIFO);
         unlink(SEC_CHILD_FIFO);
     }
-
+    sigprocmask(SIG_SETMASK, &prevMask, NULL);
     errno = savedErrno;
 }
 
@@ -180,15 +172,13 @@ int main(int argc, char **argv)
 {
     struct sigaction sa;
     sigset_t prevMask, blockMask;
-    int sig;
+    // int sig;
     int bufferLen = 256;
     char buffer[bufferLen];
     char log_buffer[bufferLen];
-    int sec_fifo_fd;
-    int status1, status2;
+
     int logSize = 0;
 
-    // char err[256]; 
     
     if(argc != 2){
         char err[256] = "Invalid argument number, must be 2.\n"; 
@@ -198,24 +188,18 @@ int main(int argc, char **argv)
     }
 
     int argument = atoi(argv[1]);
-    // int sizeofprint = snprintf(buffer, sizeof(buffer), "%d", argument);
-    // write(STDOUT_FILENO, buffer, sizeofprint);
 
-    if(argument < 0){
+    if(argument <= 0){
         char err[256] = "Argument must be greater than 0.\n";
         write(STDOUT_FILENO, err, strlen(err)); 
         memset(err, 0, sizeof(err));
         exit(EXIT_FAILURE);
     }
 
-    // fflush(STDOUT_FILENO);
     
     sigemptyset(&blockMask);
     sigaddset(&blockMask, SIGINT);
-    // sigemptyset(&sa.sa_mask);
-    // if(sigaction(SIGINT, &blockMask, NULL) == -1 ){
-    //     exit(EXIT_FAILURE);
-    // }
+
 
     if(sigprocmask(SIG_BLOCK, &blockMask, &prevMask) == -1){
         exit(EXIT_FAILURE);
@@ -248,9 +232,8 @@ int main(int argc, char **argv)
     switch(pid)
     {
         case 0 :
-        // spawnedProcess++;
-            sleep(1);
-            // int childBufferCounter = 0;
+        spawnedProcess++;
+            sleep(10);
             int childPid = getpid();//error check
             int log_written = sprintf(log_buffer, "Entered Child Process 1. (pid : %d)\n", childPid);
             char* childBuf;
@@ -258,16 +241,11 @@ int main(int argc, char **argv)
             memset(log_buffer, 0, log_written);
 
             childBuf = readOneByOne(FIRST_CHILD_FIFO);
-            // printf("\n\n\nParse Fifoya geldim \n");
             int sum = parseFifo(childBuf, ',');
 
-            int numBytes = write(STDOUT_FILENO, childBuf, strlen(childBuf));
-            if(numBytes == -1){
-                perror("write failed\n");
-                memset(childBuf, 0, strlen(childBuf));
-                free(childBuf);
-                exit(EXIT_FAILURE);
-            }
+            // int numBytes = write(STDOUT_FILENO, childBuf, strlen(childBuf));
+            int numBytes;
+  
             memset(childBuf, 0, strlen(childBuf));
             intoStringWithDelim(sum, childBuf, '!');
             numBytes = sprintf(log_buffer, "\nSum (exlamation ended): %s", childBuf);
@@ -275,7 +253,6 @@ int main(int argc, char **argv)
             memset(log_buffer, 0, strlen(log_buffer));
             // free(childBuf);
 
-            // open wronly fifo2
             int fifo2 = open(SEC_CHILD_FIFO, O_WRONLY);
             if(fifo2 == -1){
                 perror("open failed\n");
@@ -334,7 +311,7 @@ int main(int argc, char **argv)
                 perror("open failed");
                 free(str);
                 break;//not sure
-                //fail oldugunda error code ver
+
             }
             if(writeToFirstFifo(str, firstFifoFd) == -1)
             {
@@ -356,43 +333,23 @@ int main(int argc, char **argv)
                 }
                 // exit(EXIT_FAILURE);
                 break;
-                //fail oldugunda error code ver
+
             }
             // //fork
             int secPid = fork();
             switch(secPid){
                 case 0:
                 //child2
-                // spawnedProcess++;
-                    sleep(1);
+                spawnedProcess++;
+                    sleep(10);
                     // char c; 
                     char* childBuff = NULL; 
                     int pid2= getpid();
                     int log_written = sprintf(log_buffer, "\nEntered Child Process 2. (pid : %d)\n", pid2);
                     write(STDOUT_FILENO, &log_buffer, log_written);
                     memset(log_buffer, 0, log_written);
-                    // int fifo = open(SEC_CHILD_FIFO, O_RDONLY);
-                    // if(fifo == -1){
-                    //     perror("open failed");
-                    //     // free(childBuff);
-                    //     exit(EXIT_FAILURE);
-                    // }
-                    // printf("asdasdas\n");
-                    childBuff = readOneByOne_Custom(SEC_CHILD_FIFO, ',', '!');
-                    // printf("I Read From Fifo : \n");
-                    // int numBytesSecChild = write(STDOUT_FILENO, childBuff, strlen(childBuff));
-                    // if(numBytesSecChild == -1){
-                    //     perror("write failed (parent to child2)\n");
-                    //     free(childBuff);
-                    //     // close(fifo);
-                    //     exit(EXIT_FAILURE);
-                    // }
 
-                    // if(close(fifo) == -1){
-                    //     perror("close failed in fifo 2\n");
-                    //     free(childBuff);
-                    //     exit(EXIT_FAILURE);
-                    // }
+                    childBuff = readOneByOne_Custom(SEC_CHILD_FIFO, ',', '!');
 
                     if(parseSecondfifo(childBuff, ',')== -1){
                         free(childBuff);
@@ -411,14 +368,12 @@ int main(int argc, char **argv)
                     str = (char *)realloc(str, sizeof(char)*(strlen(str)+10));
                     char multip[10] = "multiply,";
                     int sizeBuffer = strlen(str);
-                    // printf("size buffer , str %s, strlen(str) %d\n", str, sizeBuffer);
                     
                     for(int i =0; i < 9; i++){
                         str[sizeBuffer+i] = multip[i];
                     }
                     str[sizeBuffer+9] = '\0';
 
-                    // printf("xI Write to fifo %s\n", str);
                     int fdSecFifo = open(SEC_CHILD_FIFO, O_WRONLY);
                     if(fdSecFifo == -1){
                         perror("open failed in (fifo2) \n");
@@ -431,7 +386,6 @@ int main(int argc, char **argv)
                         free(str);
                         break;
                     }
-                    sec_fifo_fd = fdSecFifo;
                     if(close(fdSecFifo) == -1){
                         perror("close fifo failed\n");
                         free(str);
@@ -440,47 +394,22 @@ int main(int argc, char **argv)
                     // free(str);              
                 break;
             }
-            // for(;;){
-            //     sleep(2);
-            //     //write()
-            // }
-            //sleep 
-            //waitpid koy
-            //wronly fifo2
-            int numDead= 0;
-            // for(;;){
-            //     int childpid = wait(NULL);
-            //     if(childpid == -1){
 
-            //         if(errno == ECHILD){
-            //             printf("No more children BYE \n");
-            //         }
-            //         else{
-            //             perror("wait");
-            //             exit(0);
-            //         }
-            //     }
-            //     numDead++;
-            //     printf("child exited %d\n", pid);
-            // }
-            // waitpid(pid, &status1,0);
-            // waitpid(secPid, &status2, 0);
-            // if(sigprocmask(SIG_SETMASK, &prevMask, NULL) == -1){
-            //     exit(EXIT_FAILURE);
-            // }
             sigemptyset(&prevMask);
-            while(spawnedProcess == 0) 
+            for(;;)
             {
-                
                 logSize = sprintf(log_buffer, "Proceeding...\n");
                 write(STDOUT_FILENO, log_buffer, logSize);
                 memset(log_buffer, 0, logSize);
-                if(signalCnt == spawnedProcess){
+                if(spawnedProcess==0){
                     break;
                 }
                 if (sigsuspend(&prevMask) == -1 && errno != EINTR)
                     perror("sigsuspend");
-                sleep(1);
+                sleep(2);
+                if(spawnedProcess == signalCnt){
+                    break;
+                }
             }
 
             printf("All childs are dead... Terminating...Bye\n");
